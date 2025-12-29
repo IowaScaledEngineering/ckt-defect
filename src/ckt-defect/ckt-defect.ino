e8957ab5b2671c946bd1cb8f641dc03718c2cf0c
Michael Petersen <railfan@drgw.net>
2025-12-28 17:35:49 -0700

    Task based audio feeder

--- src/ckt-defect/ckt-defect.ino
+++ src/ckt-defect/ckt-defect.ino
@@ -33,9 +33,10 @@ LICENSE:
 // 3 sec watchdog 
 #define TWDT_TIMEOUT_MS    3000
 
-// Samples for each buffer
-#define AUDIO_BUFFER_SIZE   512
-#define AUDIO_BUFFER_NUM      4
+// Frames per I2S DMA buffer
+#define I2S_NFRAMES   240
+// Number of I2S DMA buffers
+#define I2S_NBUFFERS    6
 
 i2s_chan_handle_t i2s_tx_handle;
 
@@ -292,135 +293,26 @@ void setup()
 	timerAlarm(timer, 10000, true, 0);            // 1us * 10000 = 10ms, autoreload, unlimited reloads
 }
 
-
-
-void playerInit(void)
-{
-	playerState = PLAYER_RESET;
-}
-
-void play(i2s_chan_handle_t i2s_handle)
+static void audioPump(void *args)
 {
 	int16_t sampleValue;
 	size_t bytesWritten;
 	i2s_std_clk_config_t clk_cfg;
-	// Static so the value persists between calls to play()
-	static uint32_t outputValue;  // needs to persist for PLAYER_RETRY
-	static Sound *wavSound;
-	static bool seamlessPlay;
-	static uint32_t oldSampleRate;
-	static uint32_t flushCount;
+	uint32_t outputValue;
+	Sound *wavSound = NULL;
+	bool seamlessPlay = false;
+	uint32_t oldSampleRate = 0;
+	uint32_t flushCount = 0;
+	bool delayTask = false;
 
-	esp_task_wdt_reset();
+	playerState = PLAYER_RESET;
 
-	switch(playerState)
+	while(1)
 	{
-		case PLAYER_IDLE:
-			if(NULL != wavSoundNext.wav)
-			{
-				// Queue not empty
-				playerState = PLAYER_INIT;
-			}
-			break;
-
-		case PLAYER_INIT:
-			wavSound = wavSoundNext.wav;  // Read the queue
-			seamlessPlay = wavSoundNext.seamlessPlay;
-			wavSoundNext.wav = NULL;  // Clear the queue
-//gpio_set_level(AUX5, 1);
-			wavSound->open();         // Open the sound
-//gpio_set_level(AUX5, 0);
-			if(wavSound->getSampleRate() == oldSampleRate)
-				playerState = PLAYER_PLAY;
-			else
-				playerState = PLAYER_RECONFIGURE;
-			break;
-
-		case PLAYER_RECONFIGURE:
-			clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(wavSound->getSampleRate());
-			gpio_set_level(I2S_SD, 0);             // Disable amplifier
-			i2s_channel_disable(i2s_tx_handle);  // Disable I2S
-			i2s_channel_reconfig_std_clock(i2s_tx_handle, &clk_cfg);  // Reset sample rate
-			i2s_channel_enable(i2s_tx_handle);  // Enable I2S
-			gpio_set_level(I2S_SD, 1);             // Enable amplifier
-			oldSampleRate = wavSound->getSampleRate();
-			playerState = PLAYER_PLAY;
-			break;
-
-		case PLAYER_PLAY:
-			if(stopPlayer)
-			{
-				stopPlayer = false;
-				playerState = PLAYER_FLUSH;
-			}
-			else if(wavSound->available())
-			{
-				// Sound not done, more samples available
-//gpio_set_level(AUX2, 1);
-				sampleValue = wavSound->getNextSample();
-//gpio_set_level(AUX2, 0);
-				int32_t adjustedValue = sampleValue * volume / volumeLevels[VOL_STEP_NOM];
-				if(adjustedValue > 32767)
-					sampleValue = 32767;
-				else if(adjustedValue < -32768)
-					sampleValue = -32768;
-				else
-					sampleValue = adjustedValue;
-				// Combine into 32 bit word (left & right)
-				outputValue = (sampleValue<<16) | (sampleValue & 0xffff);
-//gpio_set_level(AUX3, 1);
-				i2s_channel_write(i2s_handle, &outputValue, 4, &bytesWritten, 1);
-				if(0 == bytesWritten)
-				{
-					// Sample rejected, DMA buffer full
-					playerState = PLAYER_RETRY;
-				}
-//gpio_set_level(AUX3, 0);
-			}
-			else
-			{
-				// Sound done, no samples available
-				wavSound->close();
-				if((NULL != wavSoundNext.wav) && (seamlessPlay))
-				{
-					// Queue not empty and seamless playing, so grab next
-					playerState = PLAYER_INIT;
-				}
-				else
-				{
-					// Otherwise, flush
-					playerState = PLAYER_FLUSH;
-				}
-			}
-			break;
-
-		case PLAYER_RETRY:
-//gpio_set_level(AUX4, 1);
-			i2s_channel_write(i2s_handle, &outputValue, 4, &bytesWritten, 1);
-			if(0 == bytesWritten)
-				playerState = PLAYER_RETRY;
-			else
-				playerState = PLAYER_PLAY;
-//gpio_set_level(AUX4, 0);
-			break;
-
-		case PLAYER_FLUSH:
-			flushCount = 0;
-			#pragma GCC diagnostic push
-			#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
-			playerState = PLAYER_FLUSHING;
-			// Intentional fall through so we attempt one flush
-		case PLAYER_FLUSHING:
-			#pragma GCC diagnostic pop
-			outputValue = 0;
-			i2s_channel_write(i2s_handle, &outputValue, 4, &bytesWritten, 1);
-			if(0 != bytesWritten)
-			{
-				// Success
-				flushCount++;
-			}
-			if(flushCount >= dmaBufferSize)
-			{
+//		esp_task_wdt_reset();
+		switch(playerState)
+		{
+			case PLAYER_IDLE:
 				if(NULL != wavSoundNext.wav)
 				{
 					// Queue not empty
@@ -428,19 +320,140 @@ void play(i2s_chan_handle_t i2s_handle)
 				}
 				else
 				{
-					// Queue empty
-					playerState = PLAYER_RESET;
+					delayTask = true;
 				}
-			}
-			break;
-		
-		case PLAYER_RESET:
-			gpio_set_level(I2S_SD, 0);             // Disable amplifier
-			i2s_channel_disable(i2s_tx_handle);  // Disable I2S
-			oldSampleRate = 0;
-			playerState = PLAYER_IDLE;
-			break;
+				break;
+
+			case PLAYER_INIT:
+				wavSound = wavSoundNext.wav;  // Read the queue
+				seamlessPlay = wavSoundNext.seamlessPlay;
+				wavSoundNext.wav = NULL;  // Clear the queue
+				wavSound->open();         // Open the sound
+				if(wavSound->getSampleRate() == oldSampleRate)
+					playerState = PLAYER_PLAY;
+				else
+					playerState = PLAYER_RECONFIGURE;
+				break;
+
+			case PLAYER_RECONFIGURE:
+				clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(wavSound->getSampleRate());
+				gpio_set_level(I2S_SD, 0);             // Disable amplifier
+				i2s_channel_disable(i2s_tx_handle);  // Disable I2S
+				i2s_channel_reconfig_std_clock(i2s_tx_handle, &clk_cfg);  // Reset sample rate
+				i2s_channel_enable(i2s_tx_handle);  // Enable I2S
+				gpio_set_level(I2S_SD, 1);             // Enable amplifier
+				oldSampleRate = wavSound->getSampleRate();
+				playerState = PLAYER_PLAY;
+				break;
+
+			case PLAYER_PLAY:
+				if(stopPlayer)
+				{
+					stopPlayer = false;
+					playerState = PLAYER_FLUSH;
+				}
+				else if(wavSound->available())
+				{
+					// Sound not done, more samples available
+//					gpio_set_level(AUX2, 1);  ///////////////////////////////////////////////////////////////////////////////////////////
+					sampleValue = wavSound->getNextSample();
+//					gpio_set_level(AUX2, 0);  ///////////////////////////////////////////////////////////////////////////////////////////
+					int32_t adjustedValue = sampleValue * volume / volumeLevels[VOL_STEP_NOM];
+					if(adjustedValue > 32767)
+						sampleValue = 32767;
+					else if(adjustedValue < -32768)
+						sampleValue = -32768;
+					else
+						sampleValue = adjustedValue;
+					// Combine into 32 bit word (left & right)
+					outputValue = (sampleValue<<16) | (sampleValue & 0xffff);
+					gpio_set_level(AUX3, 1);  ///////////////////////////////////////////////////////////////////////////////////////////
+					i2s_channel_write(i2s_tx_handle, &outputValue, 4, &bytesWritten, 1);
+					gpio_set_level(AUX3, 0);  ///////////////////////////////////////////////////////////////////////////////////////////
+					if(0 == bytesWritten)
+					{
+						// Sample rejected, DMA buffer full
+						playerState = PLAYER_RETRY;
+						delayTask = true;
+					}
+				}
+				else
+				{
+					// Sound done, no samples available
+					wavSound->close();
+					if((NULL != wavSoundNext.wav) && (seamlessPlay))
+					{
+						// Queue not empty and seamless playing, so grab next
+						playerState = PLAYER_INIT;
+					}
+					else
+					{
+						// Otherwise, flush
+						playerState = PLAYER_FLUSH;
+					}
+				}
+				break;
+
+			case PLAYER_RETRY:
+gpio_set_level(AUX2, 1);  ///////////////////////////////////////////////////////////////////////////////////////////
+				i2s_channel_write(i2s_tx_handle, &outputValue, 4, &bytesWritten, 1);
+				if(0 == bytesWritten)
+				{
+					playerState = PLAYER_RETRY;
+					delayTask = true;
+				}
+				else
+					playerState = PLAYER_PLAY;
+gpio_set_level(AUX2, 0);  ///////////////////////////////////////////////////////////////////////////////////////////
+				break;
+
+			case PLAYER_FLUSH:
+				flushCount = 0;
+				#pragma GCC diagnostic push
+				#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+				playerState = PLAYER_FLUSHING;
+				// Intentional fall through so we attempt one flush
+			case PLAYER_FLUSHING:
+				#pragma GCC diagnostic pop
+				outputValue = 0;
+				i2s_channel_write(i2s_tx_handle, &outputValue, 4, &bytesWritten, 1);
+				if(0 != bytesWritten)
+				{
+					// Success
+					flushCount++;
+				}
+				if(flushCount >= dmaBufferSize)
+				{
+					if(NULL != wavSoundNext.wav)
+					{
+						// Queue not empty
+						playerState = PLAYER_INIT;
+					}
+					else
+					{
+						// Queue empty
+						playerState = PLAYER_RESET;
+					}
+				}
+				break;
+			
+			case PLAYER_RESET:
+				gpio_set_level(I2S_SD, 0);             // Disable amplifier
+				i2s_channel_disable(i2s_tx_handle);  // Disable I2S
+				oldSampleRate = 0;
+				playerState = PLAYER_IDLE;
+				break;
+		}
+
+//esp_task_wdt_reset();
+
+		if(delayTask)
+		{
+			delayTask = false;
+			vTaskDelay(2 / portTICK_PERIOD_MS);  // Block execution of this task for 2ms since we're not doing anything useful at the moment (idle or waiting for a buffer to clear)
+		}
 	}
+	vTaskDelete(NULL);
 }
 
 
@@ -666,8 +679,6 @@ void loop()
 
 	esp_task_wdt_reset();
 	
-	playerInit();
-
 	Serial.println("ISE Sound Player");
 
 	Serial.print("Version: ");
@@ -838,10 +849,16 @@ void loop()
 	// Default dma_frame_num = 240, dma_desc_num = 6 (i2s_common.h)
 	//    Total DMA size = 240 * 6 * 2 * 16 / 8 = 5760 bytes
 	i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
+	chan_cfg.dma_frame_num = I2S_NFRAMES;
+	chan_cfg.dma_desc_num = I2S_NBUFFERS;
+	Serial.print("dma_frame_num: ");
+	Serial.println(chan_cfg.dma_frame_num);
+	Serial.print("dma_desc_num: ");
+	Serial.println(chan_cfg.dma_desc_num);
 	i2s_new_channel(&chan_cfg, &i2s_tx_handle, NULL);
 
 	i2s_std_config_t std_cfg = {
-		.clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(16000),
+		.clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(44100),  // FIXME
 		.slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO),
 		.gpio_cfg = {
 			.mclk = I2S_GPIO_UNUSED,
@@ -858,6 +875,15 @@ void loop()
 	};
 
 	i2s_channel_init_std_mode(i2s_tx_handle, &std_cfg);
+
+	size_t bytesLoaded = 0;
+	uint8_t tempByte = 0;
+	do
+	{
+		esp_task_wdt_reset();
+		i2s_channel_preload_data(i2s_tx_handle, &tempByte, 1, &bytesLoaded);
+	} while(bytesLoaded);
+
 	i2s_channel_enable(i2s_tx_handle);
 
 	i2s_chan_info_t chan_info;
@@ -868,8 +894,13 @@ void loop()
 
 	gpio_set_level(I2S_SD, 1);	// Enable amplifier
 
+	gpio_set_level(AUX5, 1);  ///////////////////////////////////////////////////////////////////////////////////////////
+	xTaskCreate(audioPump, "audioPump", 8192, NULL, 24, NULL);
+	gpio_set_level(AUX5, 0);  ///////////////////////////////////////////////////////////////////////////////////////////
+
 	while(1)
 	{
+		gpio_set_level(AUX1, 1);  ///////////////////////////////////////////////////////////////////////////////////////////
 		esp_task_wdt_reset();
 
 		// Read inputs
@@ -903,6 +934,8 @@ void loop()
 		else
 			inputStatus |= EN4_INPUT;
 
+		gpio_set_level(AUX1, 0);  ///////////////////////////////////////////////////////////////////////////////////////////
+
 		// Do things on 10ms interval
 		if(timerTick)
 		{
@@ -933,8 +966,6 @@ void loop()
 		}
 
 		// Turn off volume LED
-gpio_set_level(AUX2, 1);
-gpio_set_level(AUX2, 0);
 		uint16_t ledHoldTime = (VOL_STEP_NOM == volumeStep) ? 1000 : 100;
 		if((millis() - pressTime) > ledHoldTime)
 		{
@@ -1056,26 +1087,29 @@ gpio_set_level(AUX2, 0);
 				state = SOUNDPLAYER_AMBIENT_QUEUE;
 				break;
 			case SOUNDPLAYER_AMBIENT_QUEUE:
-				Serial.print("Heap free: ");
-				Serial.println(esp_get_free_heap_size());
-
-				sampleNum = random(0, ambientSounds.size());
-				if(ambientSounds.size() > 2)
+				if(NULL == wavSoundNext.wav)
 				{
-					// With three or more sounds, don't repeat the last one
-					while(sampleNum == lastSampleNum)
+					Serial.print("Heap free: ");
+					Serial.println(esp_get_free_heap_size());
+
+					sampleNum = random(0, ambientSounds.size());
+					if(ambientSounds.size() > 2)
 					{
-						esp_task_wdt_reset();
-						sampleNum = random(0, ambientSounds.size());
-						Serial.println("*");
+						// With three or more sounds, don't repeat the last one
+						while(sampleNum == lastSampleNum)
+						{
+							esp_task_wdt_reset();
+							sampleNum = random(0, ambientSounds.size());
+							Serial.println("*");
+						}
 					}
+					Serial.print("Queueing... ");
+					Serial.println(sampleNum);
+					wavSoundNext.wav = ambientSounds[sampleNum];
+					wavSoundNext.seamlessPlay = false;
+					lastSampleNum = sampleNum;
+					state = SOUNDPLAYER_AMBIENT_PLAY;
 				}
-				Serial.print("Queueing... ");
-				Serial.println(sampleNum);
-				wavSoundNext.wav = ambientSounds[sampleNum];
-				wavSoundNext.seamlessPlay = false;
-				lastSampleNum = sampleNum;
-				state = SOUNDPLAYER_AMBIENT_PLAY;
 				break;
 			case SOUNDPLAYER_AMBIENT_PLAY:
 				if(enable)
@@ -1289,13 +1323,6 @@ gpio_set_level(AUX2, 0);
 				}
 				break;
 		}
-
-
-gpio_set_level(AUX1, 1);
-		// Pump the sound player
-		play(i2s_tx_handle);
-gpio_set_level(AUX1, 0);
-
 
 		if(restart)
 		{
