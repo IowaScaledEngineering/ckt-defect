5dc3ace223452b7fc71f61827c5329339e8fe7b3
Michael Petersen <railfan@drgw.net>
2026-05-29 21:29:42 -0600

    Fixed tag logic

--- src/git-revision.sh
+++ src/git-revision.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-revisioncount=$(git rev-list  `git rev-list --tags="[0-9][.][0-9]" --no-walk --max-count=1`..HEAD --count)
+revisioncount=$(git rev-list  `git rev-list --tags="v[0-9][.][0-9]" --no-walk --max-count=1`..HEAD --count)
 projectversion=`git describe --tags --match "v[0-9][.][0-9]" --long`
 cleanversion=${projectversion%%-*}
 
