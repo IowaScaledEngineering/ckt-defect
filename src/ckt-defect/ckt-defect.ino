555c675df3d4724fa84d941cd20165a769a94d63
Michael Petersen <railfan@drgw.net>
2026-07-11 22:19:52 -0600

    Removed debug code

--- src/ckt-defect/ckt-defect.ino
+++ src/ckt-defect/ckt-defect.ino
@@ -484,9 +484,9 @@ void loop()
 		{
 			tmpMessage = "Axle #axle";
 		}
-		trackMessages.defects.emplace_back("#tone hot journal " + tmpMessage, "hot journal " + tmpMessage, "#track\\Hot Journal\\" + tmpMessage, 500);
-		trackMessages.defects.emplace_back("#tone dragging equipment near " + tmpMessage, "dragging equipment near " + tmpMessage, "#track\\Dragging Equipment\\" + tmpMessage, 100);
-		trackMessages.defects.emplace_back("#tone high impact wheel detected " + tmpMessage, "high impact wheel detected " + tmpMessage, "#track\\High Impact Wheel\\" + tmpMessage, 200);
+		trackMessages.defects.emplace_back("#tone #track hot journal " + tmpMessage, "hot journal " + tmpMessage, "#track\\Hot Journal\\" + tmpMessage, 500);
+		trackMessages.defects.emplace_back("#tone #track dragging equipment near " + tmpMessage, "dragging equipment near " + tmpMessage, "#track\\Dragging Equipment\\" + tmpMessage, 100);
+		trackMessages.defects.emplace_back("#tone #track high impact wheel detected " + tmpMessage, "high impact wheel detected " + tmpMessage, "#track\\High Impact Wheel\\" + tmpMessage, 200);
 
 		// Create Footer
 		tmpMessage.clear();
